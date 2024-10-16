34d300096a747cadc565fcffc8630e3760928da7
Darsh <darsh30@gmail.com>
2024-10-16 18:13:55 +0530

    Delete items from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,7 +18,8 @@ cart.forEach((cartItem) => {
 
   cartSummaryHtml += 
   `
-    <div class="cart-item-container">
+    <div class="cart-item-container 
+      js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -101,6 +102,9 @@ cart.forEach((cartItem) => {
       link.addEventListener('click', () => {
         const productId = link.dataset.productId;
         removeFromCart(productId);
+
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        container.remove();
       });
     });
 });

33875471d970739101cee43f280a71e9118fdf21
Darsh <darsh30@gmail.com>
2024-10-16 17:58:27 +0530

    Add delete items from cart function when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [{
+export let cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2
 }, {
@@ -20,6 +20,14 @@ export function addToCart(productId) {
     cart.push({
       productId: productId,
       quantity: 1
+    });
+  }
+}
+
+export function removeFromCart(productId) {
+  if (cart.length !== 0) {
+    cart = cart.filter((item) => {
+      return item.productId !== productId
     });
   }
 }

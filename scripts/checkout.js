f235a0ca674b2013c7f96dd0ddb62375abdcfdc2
Darsh <darsh30@gmail.com>
2024-10-18 18:28:24 +0530

    make delivery options interactive by adding eventlistener and storing same option in storage

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart, calculateCartQuantity, updateItemQuantity} from "../data/cart.js";
+import { cart, removeFromCart, calculateCartQuantity, updateItemQuantity, updateDeliveryOption} from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
@@ -102,7 +102,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
     html += 
     `
-      <div class="delivery-option">
+      <div class="delivery-option js-delivery-option"
+      data-product-id="${matchingProduct.id}"
+      data-delivery-option-id="${deliveryOption.id}">
         <input type="radio"
           ${isChecked ? 'checked' : ''}
           class="delivery-option-input"
@@ -178,4 +180,12 @@ document.querySelectorAll('.js-save-link')
       if (event.key === 'Enter')
         saveEvent();
     });
+  });
+
+document.querySelectorAll('.js-delivery-option')
+  .forEach((element) => {
+    element.addEventListener('click', () => {
+      const {productId, deliveryOptionId} = element.dataset;
+      updateDeliveryOption(productId, deliveryOptionId);
+    })
   });
